2a710cd4451af57d1329667642b643c96bf6ee7c
Somanath singh <135754916+Somanath-Singh@users.noreply.github.com>
2023-06-26 00:46:36 +0530

    Update logic.js
    some changes done

--- logic.js
+++ logic.js
@@ -38,3 +38,68 @@ function getAmount()
             totAmt1.value = "Total Amount  "+A;
 			totAmt2.value = "Compound Intrest  "+CI;
 		}
+
+                //matrix multiplication
+
+		 //1st array declaration
+		 const mat1=[[],[],[]];
+		 //2nd array declaration
+		 const mat2=[[],[],[]];
+		 //3rd array declaration
+		 const mat3=[[],[],[]];
+		 function fun1()
+		 {
+			 //assign the value to array1 
+		 mat1[0][0]=document.getElementById("s1").value;
+		 mat1[0][1]=document.getElementById("s2").value;
+		 mat1[0][2]=document.getElementById("s3").value;
+		 mat1[1][0]=document.getElementById("s4").value;
+		 mat1[1][1]=document.getElementById("s5").value;
+		 mat1[1][2]=document.getElementById("s6").value;
+		 mat1[2][0]=document.getElementById("s7").value;
+		 mat1[2][1]=document.getElementById("s8").value;
+		 mat1[2][2]=document.getElementById("s9").value;
+		//assign the value to array2
+		 mat2[0][0]=document.getElementById("b1").value;
+		 mat2[0][1]=document.getElementById("b2").value;
+		 mat2[0][2]=document.getElementById("b3").value;
+		 mat2[1][0]=document.getElementById("b4").value;
+		 mat2[1][1]=document.getElementById("b5").value;
+		 mat2[1][2]=document.getElementById("b6").value;
+		 mat2[2][0]=document.getElementById("b7").value;
+		 mat2[2][1]=document.getElementById("b8").value;
+		 mat2[2][2]=document.getElementById("b9").value;
+ 
+		 for(let i=0;i<mat1.length;i++){
+			 for(let j=0;j<mat2.length;j++){
+				 document.write(`${mat1[i][j]} &nbsp`);
+			 }
+			 document.write("<br>");
+		 }
+		 for(let i=0;i<mat2.length;i++){
+			 for(let j=0;j<mat2.length;j++){
+				 document.write(`${mat2[i][j]} &nbsp`);
+			 }
+			 document.write("<br>");
+		 }
+ 
+		 //Multiplication of two matrix
+	 for(let i=0;i<mat1.length;i++)
+	 {
+		 for(let j=0;j<mat2.length;j++)
+		 {
+			mat3[i][j]=0;
+			for(let k=0;k<3;k++)
+			{
+				mat3[i][j]=mat3[i][j]+mat1[i][k]*mat2[k][j];
+			}
+		 }
+	 }
+	 for(let i=0;i<mat2.length;i++){
+			 for(let j=0;j<mat2.length;j++){
+				 document.write(`${mat3[i][j]} &nbsp`);
+			 }
+			 document.write("<br>");
+		 }
+		 
+		 }
